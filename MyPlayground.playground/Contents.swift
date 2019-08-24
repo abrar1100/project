7b929905243697df931c79b204228b7ef6c12945
Abrar1100 <a.a.-2010-@hotmail.com>
2019-08-24 17:41:23 +0300

    my commit

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var eggPrice: Int = 15
+var amountOfEggsBought: Int = 3
+var totalAmount = eggPrice * amountOfEggsBought
+print("subtotal: \(totalAmount)")
+// Output: subtotal: 45
+var Salary_person1=5000
+var Home_person1=1000*12
+var Live_person1=700*12
+var Other_person1=1300*12
+
+var Salary_annual=Salary_person1*12
+var Other_annual=Home_person1+Live_person1+Other_person1
+
+var saving_annual=Salary_annual-Other_annual
+print("Saving annual=\(saving_annual)")
